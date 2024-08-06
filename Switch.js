a704d228fb95fd24d9a8c8b7a2d104c781c63cbb
TANNU <161803939+TamannaNag@users.noreply.github.com>
2024-08-06 12:28:09 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,115 @@
+/*
+let day = "monday"
+
+  switch(day){
+    case 'monday':
+        console.log("7am monday" );
+        break;
+        case 'tuesday': 
+            console.log("3am tuesday");
+            break;
+            case 'wednesday':
+                console.log("4am wednesday");
+                break;
+                case 'thursday':
+                    console.log("5am thursday");
+                    break;
+                    case 'friday':
+                        console.log("6am friday");
+                        break;
+                        case 'saturday':
+                            console.log("7am saturday");
+                            break;
+                            case 'sunday':
+                                console.log("8am sunday");
+
+  }
+  */
+
+
+  //---------------------------------------------------------------------------------------------
+ 
+//   let light = 'green';
+
+//   switch(light) {
+//       case 'red':
+//           console.log("Stop");
+//           break;
+//       case 'yellow':
+//           console.log("Caution");
+//           break;
+//       case 'green':
+//           console.log("Go");
+//           break;
+//       default:
+//           console.log("Invalid light color");
+//   }
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+  /*
+Write a switch statement that takes a number (1-3) and prints "One", "Two", or "Three" accordingly*/
+
+
+/*
+  let number = 3;
+switch (number) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    default:
+        console.log("Number not in range");
+}
+*/
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+
+Write a switch statement to perform addition, subtraction, multiplication, or division based on an operator (+, -, *, /) and two operands.
+*/
+
+
+// let a = 10;
+// let b = 20;
+// let operator = '%'
+// switch (operator) {
+//     case '+':
+//         console.log(a+b);
+        
+//         break;
+//     case '-':
+//         console.log(a-b);
+//         break;
+//     case '*' :
+//         console.log(a*b);
+//         break;
+//     case '/':
+//         console.log(a/b);
+//         break;
+    
+//     case '%' :
+//         console.log(a%b);
+//         break;
+//     case '**' :
+//         console.log(a**b);
+//         break;
+    
+//     default:
+//         console.log("its not a operator");
+        
+// }
+
+
+
+
